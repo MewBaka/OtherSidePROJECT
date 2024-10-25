f65f0d56cfa91a63644b729e2c9f949db9434a7f
helloyork <helloyork@icloud.com>
2024-10-24 21:39:11 -0700

    upgrade to narraleaf-react-v0.1.1

--- renderer/app/(main)/layout.tsx
+++ renderer/app/(main)/layout.tsx
@@ -4,14 +4,14 @@ import clsx from "clsx";
 import React from "react";
 
 export default function Layout({
-                                   children,
-                               }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <>
             <div
-                className={clsx("bg-[url('/static/images/main-menu-background.png')] flex bg-cover bg-center h-full w-full")}>
+                className={clsx("bg-[url('/static/images/main-menu-background.png')] flex bg-cover bg-center h-full w-full absolute")}>
                 {children}
             </div>
         </>
---
+++ renderer/lib/game/story/definitions/definitions.ts
@@ -0,0 +1,16 @@
+import {Scene} from "narraleaf-react";
+import {Character} from "narraleaf-react";
+import {Sound, SoundType} from "narraleaf-react";
+import {Image} from "narraleaf-react";
+import {Transform} from "narraleaf-react";
+import type {TransformDefinitions} from "narraleaf-react";
+import {Fade} from "narraleaf-react";
+import {Align, Coord2D} from "narraleaf-react";
+
+export const scene1 = new Scene("scene1", {
+    background: "#E4E4E3",
+    invertY: true,
+});
+
+export {
+};
